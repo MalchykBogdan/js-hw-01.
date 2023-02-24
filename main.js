b348c3fae749b9c28b976ada41972b6b59c3ee2a
MalchykBogdan <124812110+MalchykBogdan@users.noreply.github.com>
2023-02-24 20:52:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+//Перше завдання
+
+
+//const name = 'Генератор' 
+//const price =  1000
+//const newPrice = 2000
+
+//console.log(`Обрано ${name}, ціна за одиницю ${price} кредитів`);
+//console.log(`Обрано ${name}, ціна за одиницю ${newPrice} кредитів`);
+
+
+//Друге Завдання
+
+
+
+// let total = 130; 
+// const ordered = 100; 
+
+// if (total > ordered){
+//    total -= ordered
+//    alert(`Замовлення оформлено!`)
+// }
+// else{
+//    alert(`На складі недостатня кількість товару!`)
+// }
+
+
+
+//Третє завдання
+
+
+
+
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+// let message;
+
+// let userPassword = prompt('Введіть свій пароль:');
+
+// if (userPassword === null) {
+//   message = 'Операцію скасовано!';
+// } else if (userPassword === ADMIN_PASSWORD) {
+//   message = 'Ласкаво просимо!';
+// } else {
+//   message = 'Доступ заборонено, невірний пароль!';
+// }
+
+// alert(message);
+
+
+// Четверте Завдання
+
+
+// const credits = 23580
+// const pricePerDroid = 3000
+
+// let message;
+
+// let write = prompt('Виберіть кількість товару:');
+
+// if (write === null) {
+//   message = 'Операцію скасовано!';
+// } else if (credits >= pricePerDroid * write) {
+//   message = `Ви купили ${write} телевізорів, на рахунку залишилось ${credits-(write*pricePerDroid)} кредитів.`;
+// } else {
+//     message =`Недостатньо коштів на рахунку!`
+// }
+
+// alert(message);
