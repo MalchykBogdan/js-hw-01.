9a5c535f4c6f3a98bf5262a11c29ddde734b755e
MalchykBogdan <124812110+MalchykBogdan@users.noreply.github.com>
2023-02-26 15:54:24 +0200

    m

---
+++ script_two.js
@@ -0,0 +1,14 @@
+//Друге Завдання
+
+
+
+let total = 130; 
+const ordered = 100; 
+
+if (total > ordered){
+   total -= ordered
+   alert(`Замовлення оформлено!`)
+}
+else{
+   alert(`На складі недостатня кількість товару!`)
+}
